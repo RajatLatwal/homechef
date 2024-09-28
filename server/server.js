b05d6b0fec17bc54c0bca9a79f8f02e26e9d5237
Rajat Latwal <rajatlatwal7@gmail.com>
2024-09-28 20:44:56 +0530

    changed client url in server

--- server/server.js
+++ server/server.js
@@ -5,13 +5,12 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./connection");
 const routes = require("./routes");
-const path = require("path");
 require("dotenv").config();
 
-connectDB();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
+connectDB();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 //mern project deploy process
 // app.get("/", (req, res) => {
