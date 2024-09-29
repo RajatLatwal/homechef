f25eebbca3b465da48609324848d6dc78ff6853d
Rajat Latwal <rajatlatwal7@gmail.com>
2024-09-29 18:16:33 +0530

    changed client url in server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 connectDB();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api", routes);
